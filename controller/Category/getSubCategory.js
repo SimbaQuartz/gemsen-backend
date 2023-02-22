02d4e9e9f000f992e9d64ff6d3a8a2d4c4d1e1b0
= <65956399+Bhavika0510@users.noreply.github.com>
2023-02-22 10:05:17 +0530

    updated code

--- controller/Category/getSubCategory.js
+++ controller/Category/getSubCategory.js
@@ -7,27 +7,10 @@ const getSubCategory = async (req, res, next) => {
   try {
     const { id } = req.params;
 
-    const data = await SubCategory.aggregate([
-      {
-        $match: { categoryId: ObjectId(id) },
-      },
-      {
-        $lookup: {
-          from: "category", // join from which model (mongodb schema name)
-          localField: "categoryId", // jis model ke andar hain hmlog, uski ID jo jijn krni hai , foreign field se
-          foreignField: "_id", // id from which we are joining
-          as: "categoryDetails", // return data
-        },
-      },
-      {
-        $unwind: {
-          path: "$categoryDetails",
-        },
-      },
-    ]);
+    const data = await SubCategory.find({ categoryId: ObjectId(id) });
 
     res.json({
-      message: "Delete succesfully",
+      message: "Fetched succesfully",
       data,
     });
   } catch (err) {
