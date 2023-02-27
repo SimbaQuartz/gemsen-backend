51da6acad4d82106e3af0db8361e8706e037ca56
= <65956399+Bhavika0510@users.noreply.github.com>
2023-02-27 10:05:02 +0530

    updated code

--- controller/Category/getSubCategory.js
+++ controller/Category/getSubCategory.js
@@ -7,7 +7,19 @@ const getSubCategory = async (req, res, next) => {
   try {
     const { id } = req.params;
 
-    const data = await SubCategory.find({ categoryId: ObjectId(id) });
+    const data = await SubCategory.aggregate([
+      {
+        $match: { categoryId: ObjectId(id) },
+      },
+      {
+        $lookup: {
+          from: "nestedSubCategory",
+          localField: "_id",
+          foreignField: "subCategoryId",
+          as: "nestedSubCategory",
+        },
+      },
+    ]);
 
     res.json({
       message: "Fetched succesfully",
